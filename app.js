a108bf68cff6a87ce1c14247e7b230d0ff3101fc
Jameson Farmer <thejamesonfarmer@gmail.com>
2026-03-03 14:09:12 -0800

    adding sql and database connections

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import mysql2 from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config()
 const app = express();
 const PORT = 3000;
 app.use(express.static('public'));
@@ -10,8 +14,25 @@ app.set('view engine', 'ejs');
 // form data and store it in req.body
 app.use(express.urlencoded({ extended: true }));
 
-// Create a temp array to store orders
-const orders = []; 
+
+//create db pool of db connections
+const pool = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+}).promise();
+
+//db test route 
+app.get('/dbtest', async (req, res) => {
+    try {
+        const pizza_orders = await pool.query('SELECT * FROM orders');
+        res.send(pizza_orders[0]);
+    }catch(err) {
+        console.log(`Database Error: ${err}`)
+    }
+});
 
 // Default route
 app.get('/', (req, res) => {
@@ -29,30 +50,38 @@ app.get('/thank-you', (req, res) => {
 });
 
 // Admin route
-app.get('/admin', (req, res) => {
-    res.render('admin', { orders });
+app.get('/admin', async (req, res) => {
+    //read all orders from the database 
+    // newest first 
+    const orders = await pool.query("SELECT * FROM orders ORDER BY timestamp DESC")
+    console.log(orders);
+
+   res.render('admin', { orders: orders[0] });
 });
 
 // Submit order route
 // {"fname":"a","lname":"aa","email":"a",
 // "method":"delivery","toppings":["artichokes"],
 // "size":"small","comment":"","discount":"on"}
-app.post('/submit-order', (req, res) => {
+app.post('/submit-order', async (req, res) => {
+    const order = req.body
     
-    // Create a JSON object to store the order data
-    const order = {
-        fname: req.body.fname,
-        lname: req.body.lname,
-        email: req.body.email,
-        method: req.body.method,
-        toppings: req.body.toppings ? req.body.toppings : "none",
-        size: req.body.size,
-        comment: req.body.comment,
-        timestamp: new Date()
-    };
+    // Create an array to store the order data
+    const params = [
+        order.fname,
+        order.lname,
+        order.email,
+        order.size,
+        order.method,
+        Array.isArray(req.body.toppings) ? req.body.toppings.join(",") : "none"
+    ];
 
-    // Add order object to orders array
-    orders.push(order);
+    //insert a new order into the db
+    const sql = `INSERT INTO orders (fname, lname, email, method, toppings, size) VALUES (?, ?, ?, ?, ?, ?)`
+
+    const result = await pool.execute(sql, params);
+
+    console.log(result);
 
     res.render("confirmation", { order });
 });
